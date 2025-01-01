258daebc0e206180c91e63f41871c4a05a976a4b
Myburakoz <163747149+Myburakoz@users.noreply.github.com>
2025-01-01 14:46:41 +0300

    .
    .

---
+++ Professor.h
@@ -0,0 +1,31 @@
+#ifndef PROFESSOR_H
+#define PROFESSOR_H
+#include "Person.h"
+
+class Professor: public Person
+{
+    private:
+    string* courseProf;
+    unsigned int sizeCourseProf;
+
+    public:
+    Professor();
+    Professor(string, string, string*, unsigned int);
+    Professor(const Professor &obj);
+    ~Professor();
+};
+
+Professor::Professor()
+:Person()
+{
+    this->courseProf = nullptr;
+    this->sizeCourseProf = 0;
+}
+
+Professor::Professor(string password, string username, string *, unsigned int)
+:Person(password, username)
+{
+    
+}
+
+#endif
