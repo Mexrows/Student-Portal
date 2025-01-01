ef0101f42df10abae2b042e2cfcfc947c46aa630
Mert <bolukbasmert@hotmail.com>
2025-01-01 15:32:19 +0300

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,12 +1,38 @@
-#ifndef COURSE_H
-#define COURSE_H
+#ifndef COURSE.H
+#define COURSE.H
+#include <string>
+
+using namespace std;
+
 
 class Course
 {
+    private:
+    string name;
+    int id;
 
+    public:
+    Course();
+    ~Course();
+    Course(const Course &obj);
+    Course(string, int);
+
+    string getName()
+    {
+        return this->name;
+    }
+
+    int getId()
+    {
+        return this->id;
+    }
 };
 
-
+Course::Course(string name, int id)
+{
+    this->name = name;
+    this->id = id;
+}
 
 
 
